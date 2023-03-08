4e384cc6ca735bdbbca37c1c0e019b2c38b0e17e
darthcurtiscass <114704942+darthcurtiscass@users.noreply.github.com>
2023-03-08 00:30:40 -0700

    created local storage

--- assets/script.js
+++ assets/script.js
@@ -1,120 +1,163 @@
 let questions = [
-    {
-        q:"As of 2022, what percentage of websites use Javascript for client side behavior?",
-        options: ["23.5%", "98%", "84%"],
-        answer: "b"
-    },
-    {
-        q: "What do you need to add to your HTML page in order for your Javascript to have an affect on your webpage?",
-        options: ["a", "b", "c"],
-        answer: "c"
-    },
-    {
-        q: "Which of the following statements is true?",
-        options: ["a", "b", "c"],
-        answer: "a"
-    },
-    {
-        q: "What does a queryselector need to include?",
-        options: ["a", "b", "c"],
-        answer: "b"
-    }
-]
+  {
+    q: "As of 2022, what percentage of websites use Javascript for client side behavior?",
+    options: ["23.5%", "98%", "84%"],
+    answer: "98%",
+  },
+  {
+    q: "What do you need to add to your HTML page in order for your Javascript to have an affect on your webpage?",
+    options: [
+      "A smiley face",
+      "Relative path to the javascript file",
+      "An algorithm to interpret and display the javascript",
+    ],
+    answer: "Relative path to the javascript file",
+  },
+  {
+    q: "Which of the following statements is true?",
+    options: [
+      "Javascript can generate HTML elements",
+      "HTML, CSS, cand Javascript are different coding languages and therfore cannot function together",
+      "Javascript cannot be used to make video games",
+    ],
+    answer: "Javascript can generate HTML elements",
+  },
+  {
+    q: "What does a querySelector need to include?",
+    options: [
+      "Link to the HTML page",
+      "Corresponding HTML class or id from the HTML page",
+      "Algorithm that generates new CSS elements",
+    ],
+    answer: "Corresponding HTML class or id from the HTML page",
+  },
+  {
+    q: "",
+    options: [],
+    answer: "",
+  },
+];
 
-let startButton = document.getElementById('buttonSize')
-// let buttons = document.querySelectorAll('button')
-let currentQ = 0
-var optionList = document.querySelector('.options')
-var qHeader = document.querySelector('.qHeader')
+let startButton = document.getElementById("buttonSize");
+let currentQ = 0;
+var optionList = document.querySelector(".options");
+var qHeader = document.querySelector(".qHeader");
+var q4 = questions[4];
 
-// function updateQuestion(currentQuestionIndex) {
-//     var el = document.getElementById('question')
-//     el.textContent = questions[currentQuestionIndex]
-// }
+var timerEl = document.querySelector(".timer");
+var finalTime = document.querySelector("#time");
+var timeLeft = 90;
+timerEl.textContent = timeLeft;
+var timeInterval;
+
+var scoreKeeper = 0;
+var scoreEl = document.querySelector(".score");
+var finalScore = document.querySelector("#finalScore");
+
+var nameText = document.querySelector("#name");
 
 function startQuiz() {
-    document.querySelector(".header").classList.add("hide")
-    document.querySelector(".questionElements").classList.remove("hide")
-    // document.querySelector(".button").classList.remove("hide")
-    updateQuestion(currentQ)
+  document.querySelector(".header").classList.add("hide");
+  document.querySelector(".questionElements").classList.remove("hide");
+  updateQuestion(currentQ);
+  timeInterval = setInterval(function () {
+    if (timeLeft > 1 && questions[currentQ] != questions[4]) {
+      timerEl.textContent = timeLeft + "seconds";
+
+      timeLeft--;
+    } else if (questions[currentQ] === questions[4]) {
+      endCountdown();
+    } else if (timeLeft <= 0) {
+      endCountdown();
+    }
+  }, 1000);
+  //   countdown();
+  console.log(questions[4]);
 }
 
 function updateQuestion(currentQuestionIndex) {
-   if (currentQuestionIndex > questions.length) {
-    document.querySelector(".questionElements").classList.add("hide")
-   } else {
-    qHeader.textContent = questions[currentQuestionIndex].q
-    // var question = document.querySelector('.questionElements')
-    // question.textContent = questions[currentQuestionIndex]
+  if (currentQuestionIndex > questions.length) {
+    document.querySelector(".questionElements").classList.add("hide");
+  } else {
+    qHeader.textContent = questions[currentQuestionIndex].q;
+
     for (let i = 0; i < questions[currentQuestionIndex].options.length; i++) {
-        const option = questions[currentQuestionIndex].options[i];
-        var liOption = document.createElement('li')
-        liOption.textContent = option
-        optionList.append(liOption)
+      const option = questions[currentQuestionIndex].options[i];
+      var liOption = document.createElement("button");
+      liOption.textContent = option;
+      optionList.append(liOption);
     }
-    }
+  }
 }
 
-function cleanUpQuestion () {
-    qHeader.innerHTML = ''
-    optionList.innerHTML = ''
+function cleanUpQuestion() {
+  qHeader.innerHTML = "";
+  optionList.innerHTML = "";
 }
 
-function optionChoice (event) {
-    var target = event.target
-    var choice = target.innerHTML
-    console.log(choice)
-    confirmChoice(choice)
+function optionChoice(event) {
+  var target = event.target;
+  var choice = target.innerHTML;
+  console.log(choice);
+  confirmChoice(choice);
 }
 
-function confirmChoice (selection) {
-    if (selection === questions[currentQ].answer) {
-        console.log("correct!")
-    } else {
-        console.log("wrong")
-    }
-    currentQ++
-    cleanUpQuestion();
-    updateQuestion(currentQ)
+function confirmChoice(selection) {
+  if (selection === questions[currentQ].answer) {
+    console.log("correct!");
+    scoreKeeper += 1;
+  } else {
+    console.log("wrong");
+    scoreKeeper -= 1;
+  }
+  scoreEl.textContent = scoreKeeper;
+  currentQ++;
+  cleanUpQuestion();
+  updateQuestion(currentQ);
 }
 
-optionList.addEventListener('click', optionChoice)
-startButton.addEventListener('click', startQuiz) 
+function endCountdown() {
+  // timerEl.textContent = '';
+  var message = "Your Score: ";
+  var message2 = "Your Time: ";
 
-// let questions = [
-//     "Do you like cats?",
-//     "Do you like dogs?"
-// ]
+  clearInterval(timeInterval);
 
-// let buttons = document.querySelectorAll('button')
-// let currentI = 0
+  scoreEl.textContent = "";
+  timerEl.textContent = "";
+
+  var scoreMessage = document.createElement("h3");
+  scoreMessage.textContent = message + scoreKeeper;
+  finalScore.appendChild(scoreMessage);
+
+  var timeMessage = document.createElement("h4");
+  timeMessage.textContent = message2 + timeLeft;
+  finalTime.append(timeMessage);
 
 
+  var entry = document.createElement("input")
+  var submit = document.createElement("button")
+  var label = document.createElement("h5")
+  
+  label.textContent = "Enter Your name "
 
-// updateQuestion(currentI)
+  entry.setAttribute("type", "text")
 
-// for (let i = 0; i < buttons.length; i++) {
-//     buttons[i].addEventListener('click', function() {
-//         currentI++
-//         updateQuestion(currentI)
-//     })
-// }
+  submit.textContent = "Submit"
 
-//compare choice to correct answer
+  nameText.append(label)
+  nameText.append(entry)
+  nameText.append(submit)
 
-// updateQuestion(currentQ);
-// for (let i = 0; i < opt.length; i++) {
-//     opt[i].addEventListener('click', () => {
-//         // var liOption = document.createElement('li')
-//         // liOption.appendChild(optionList)
-//         // currentQ++
-//         // updateQuestion(currentQ)
-//     })
-// }
+  entry.addEventListener("input", function() {
+    // Get the value of the input field
+    var inputText = entry.value;
+  
+    // Save the value to local storage
+    localStorage.setItem("myInputValue", inputText);
+    localStorage.setItem("myscore", scoreKeeper)
+  });
+}
 
-// for (let i = 0; i < buttons.length; i++) {
-//     buttons[i].addEventListener('click', () => {
-//         currentQ++
-//         updateQuestion(currentQ)
-//     })
-// }
+optionList.addEventListener("click", optionChoice);
+startButton.addEventListener("click", startQuiz);
